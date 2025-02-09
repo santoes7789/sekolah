0ea1763c8205aed1cd767b8848083d7caaf5eaba
AJ <faronsantoso105@gmail.com>
2025-02-10 09:44:41 +1300

    version 2

--- component_1_welcome_message.py
+++ component_1_welcome_message.py
@@ -9,4 +9,6 @@ index = random.randint(0, len(bot_names))
 
 name = bot_names[index]
 
-print(name)
+print("***Welcome to Dream Pizza***")
+print("***My name is", name, "***")
+print("***I will be here to help you order your delicious Dream Pizza***")
