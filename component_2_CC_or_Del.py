5d46985344bd52bce27fe5003549554513574529
AJ <faronsantoso105@gmail.com>
2025-02-10 09:49:47 +1300

    version 1 for comp 2

---
+++ component_2_CC_or_Del.py
@@ -0,0 +1,11 @@
+print("Do you want click and collect or delivery?")
+print("Enter 1 for click and collect")
+print("Enter 2 for delivery")
+
+choice = int(input("Please enter 1 or 2"))
+if choice == 1:
+    print("click and collect")
+elif choice == 2:
+    print("delivery")
+else:
+    print("error")
